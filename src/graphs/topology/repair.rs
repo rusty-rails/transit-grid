bae62ec72cefae6c5a0fcde4656d90c5bfc7bd2d
Christian M <chriamue@gmail.com>
2023-06-08 17:49:25 +0200

    :sparkles: implements cross link dual edge

--- src/graphs/topology/repair.rs
+++ src/graphs/topology/repair.rs
@@ -35,21 +35,31 @@ impl TopologyGraphRepairer for TopologyGraph {
         }
     }
 
-    fn cross_link_dual_edge(&mut self, _node1: NodeId, _node2: NodeId) {
-        unimplemented!("Cross-linking dual edges is not yet implemented")
+    fn cross_link_dual_edge(&mut self, node1: NodeId, node2: NodeId) {
+        if let Some(edges) = self.find_edge_indices(node1, node2) {
+            let (source1, target1) = self.graph.edge_endpoints(edges.0).unwrap();
+            let (source2, target2) = self.graph.edge_endpoints(edges.1).unwrap();
+
+            let weight1 = self.graph.edge_weight(edges.0).unwrap().clone();
+            let weight2 = self.graph.edge_weight(edges.1).unwrap().clone();
+
+            self.graph.remove_edge(edges.0);
+            self.graph.remove_edge(edges.1);
+
+            self.graph.add_edge(source1, source2, weight1);
+            self.graph.add_edge(target1, target2, weight2);
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::prelude::TopoEdge;
     use petgraph::stable_graph::EdgeIndex;
 
-    use crate::prelude::TopoEdge;
-
-    use super::*;
-
     #[test]
-    fn test_topology_graph() {
+    fn test_reverse_dual_edge() {
         let mut topo_graph = TopologyGraph::new();
 
         let node_id1 = 1;
@@ -91,5 +101,56 @@ mod tests {
 
         assert_eq!(topo_graph.has_incoming(added_node_id2_2), true);
         assert_eq!(topo_graph.has_incoming(added_node_id1_2), false);
+    }
+
+    #[test]
+    fn test_cross_link_dual_edge() {
+        let mut topo_graph = TopologyGraph::new();
+
+        let node_id1 = 1;
+        let node_id2 = 2;
+
+        let (added_node_id1_1, added_node_id1_2) = topo_graph.add_node(node_id1);
+        let (added_node_id2_1, added_node_id2_2) = topo_graph.add_node(node_id2);
+
+        assert_eq!(topo_graph.graph.node_count(), 4);
+
+        let edge_id1 = 1;
+
+        topo_graph.add_edge(edge_id1, node_id1, node_id2);
+
+        assert_eq!(topo_graph.graph.edge_count(), 2);
+
+        assert_eq!(topo_graph.has_incoming(added_node_id1_1), false);
+        assert_eq!(topo_graph.has_incoming(added_node_id2_1), true);
+
+        assert_eq!(topo_graph.has_incoming(added_node_id1_2), true);
+        assert_eq!(topo_graph.has_incoming(added_node_id2_2), false);
+
+        assert!(topo_graph
+            .graph
+            .find_edge(added_node_id1_1, added_node_id2_1)
+            .is_some());
+
+        topo_graph.cross_link_dual_edge(node_id1, node_id2);
+
+        assert_eq!(topo_graph.has_incoming(added_node_id1_1), false);
+        assert_eq!(topo_graph.has_incoming(added_node_id2_1), false);
+
+        assert_eq!(topo_graph.has_incoming(added_node_id1_2), true);
+        assert_eq!(topo_graph.has_incoming(added_node_id2_2), true);
+
+        assert!(topo_graph
+            .graph
+            .find_edge(added_node_id1_1, added_node_id2_1)
+            .is_none());
+        assert!(topo_graph
+            .graph
+            .find_edge(added_node_id2_1, added_node_id1_2)
+            .is_some());
+        assert!(topo_graph
+            .graph
+            .find_edge(added_node_id1_1, added_node_id2_2)
+            .is_some());
     }
 }
